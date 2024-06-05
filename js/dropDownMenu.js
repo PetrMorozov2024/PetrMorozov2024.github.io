f5e8ed1486073f0430d09be8135c259710b8ea5e
PetrMorozov2024 <113279269+PetrMorozov2024@users.noreply.github.com>
2024-06-05 23:25:50 +0800

    Add files via upload

---
+++ js/dropDownMenu.js
@@ -0,0 +1,24 @@
+(function(){
+  window.addEventListener('load', initialization)
+  function initialization(){
+    document.getElementById("dropButton").addEventListener("click", dropMenu)
+  }
+  function dropMenu() {
+      document.getElementById("dropDown").classList.toggle("show");
+  }
+
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropButton')) {
+
+      var dropdowns = document.getElementsByClassName("dropElements");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+})();
